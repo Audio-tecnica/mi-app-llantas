dd4a01e010e101276e5242c65abcd64c68949dca
Audio Tecnica <audio@tecnica.com>
2025-10-22 10:02:58 -0500

    cambiando logo

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -28,7 +28,7 @@ function LoginPage() {
         alignItems: 'center',
         backgroundImage: "url('/logoLogin.jpg')",
         backgroundSize: 'auto 90px',
-        backgroundPosition: '50% 28%',
+        backgroundPosition: '60% 28%',
         backgroundRepeat: 'no-repeat'
       }}
     >

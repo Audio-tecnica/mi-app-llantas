2b3820a70390382f09afb1f00f77d2c27415a186
Audio Tecnica <audio@tecnica.com>
2025-10-23 11:54:06 -0500

    actalizando 2.54

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -158,8 +158,10 @@ const filtradas = llantas.filter(l => {
   return (
     <div className="max-w-7xl mx-auto p-4">
       <div className="flex justify-between items-center mb-6 flex-wrap gap-2">
-        <img src="/logoLogin.jpg" className="h-13 w-48" />
-        <div className="flex flex-wrap gap-2">
+        <div className="bg-white p-4 rounded-lg shadow-lg">
+           <img src="/logoLogin.jpg" className="h-13 w-48" alt="Logo" />
+        </div>
+
           {/*<Link to="/subir" className="bg-blue-600 text-white px-4 py-2 rounded">Subir archivo</Link>*/} {/*PAUSADO EL LINK DE SUBIR ARCHIVO*/}
           <button onClick={() => setMostrarModal(true)} className="bg-gray-700 text-white px-3 py-1.5 rounded text-sm hover:bg-gray-800">Agregar ítem</button>
           <button onClick={handleEliminarMultiples} disabled={seleccionadas.length === 0} className="bg-red-600 text-white px-3 py-1.5 rounded text-sm hover:bg-red-700">Eliminar seleccionados</button>

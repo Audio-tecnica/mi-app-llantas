8b988749843153562d31f5c773c4aef86d11a618
Audio Tecnica <audio@tecnica.com>
2025-10-28 15:12:11 -0500

    redirige / al login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import SubirArchivo from './SubirArchivo';
 import Protegido from './Protegido';
@@ -11,13 +11,24 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Routes>
+      {/* Ruta principal redirige al login */}
+      <Route index element={<Navigate to="/login" replace />} />
+
+      {/* Login */}
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<Protegido><App /></Protegido>} />
+
+      {/* App protegida */}
+      <Route path="/home" element={<Protegido><App /></Protegido>} />
+
+      {/* Subir archivo */}
       <Route path="/subir" element={<Protegido><SubirArchivo /></Protegido>} />
-      <Route path="*" element={<LoginPage />} />
+
+      {/* Cualquier ruta desconocida también redirige al login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   </HashRouter>
 );
+
 
 
 

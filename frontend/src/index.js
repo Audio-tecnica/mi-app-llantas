4501ebeacbba365a9ee71ab474a67c784179cb5f
Audio Tecnica <audio@tecnica.com>
2025-10-28 15:07:47 -0500

    Fix rutas Vercel React Router 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import SubirArchivo from './SubirArchivo';
 import Protegido from './Protegido';
 import LoginPage from './LoginPage';
-import './index.css'
-
-
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<Protegido><App /></Protegido>} />
       <Route path="/subir" element={<Protegido><SubirArchivo /></Protegido>} />
+      <Route path="*" element={<LoginPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
-
-// 👇 Esto ya registra el SW correctamente
-serviceWorkerRegistration.register();
 
 
 

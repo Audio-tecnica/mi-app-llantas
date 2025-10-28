f4233c5d2fa0b9ede86039f79874aae7c843d53e
Audio Tecnica <audio@tecnica.com>
2025-10-28 12:06:52 -0500

    Actualizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Eye, EyeOff } from "lucide-react";
-import { Link } from 'react-router-dom';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './index.css';
 

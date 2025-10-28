054f83bdda5e4c17ca39edc98f93784480db0ef6
Audio Tecnica <audio@tecnica.com>
2025-10-28 15:35:52 -0500

    Cambia BrowserRouter a HashRouter para evitar pantalla en blanco

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import SubirArchivo from './SubirArchivo';
 import Protegido from './Protegido';

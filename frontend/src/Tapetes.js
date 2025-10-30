74affdcdae1c1d717d43d1b5fdf5a811d724ae1a
Audio Tecnica <audio@tecnica.com>
2025-10-30 14:27:29 -0500

    Acualizando color tapetes.js

--- frontend/src/Tapetes.js
+++ frontend/src/Tapetes.js
@@ -165,7 +165,7 @@ function Tapetes() {
 
   // 🧩 Render
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4 min-h-screen bg-gradient-to-b from-gray-200 to-blue-100">
       <div className="flex justify-between items-center mb-6 flex-wrap gap-2">
         <img src="/logowp.PNG" className="h-13 w-48" />
         <div className="flex flex-wrap gap-2">

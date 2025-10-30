19ba820f0e8a481afffdf159b9415f5131feed12
Audio Tecnica <audio@tecnica.com>
2025-10-30 14:47:36 -0500

    Mejorado Tapetes.js con fondo personalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -225,7 +225,7 @@ function App() {
 
   // 🧩 Render principal
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7x2 mx-auto p-4">
       {/* Encabezado */}
       <div className="flex justify-between items-center mb-6 flex-wrap gap-2">
         <img src="/logowp.PNG" className="h-13 w-48" />
